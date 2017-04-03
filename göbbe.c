eac2e7f1fedc2e5091297a7b487bf018b54fda3b
viktoralmerskar <viktor.almerskar@gmail.com>
2017-04-03 13:29:49 +0200

    tecken

--- göbbe.c
+++ göbbe.c
@@ -1,18 +1,49 @@
-0b00001010;
-0b00010001;
-0b00011111;
-0b00000000;
-0b00000000;
-0b00000000;
-0b00000000;
-0b00000000;
+void charachters() {
+  LCD_RS=0;
+  LCD_Cmd(64);
+  LCD_RS=1;
 
+  // Smiley logisk gravitation
+  LCD_Chr_Cp(0b00001010);
+  LCD_Chr_Cp(0b00010001);
+  LCD_Chr_Cp(0b00011111);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
 
-0b00000000;
-0b00000000;
-0b00000000;
-0b00000000;
-0b00000000;
-0b00011111;
-0b00010001;
-0b00001010;
+  // Smiley ologisk gravitation
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00011111);
+  LCD_Chr_Cp(0b00010001);
+  LCD_Chr_Cp(0b00001010);
+
+  // Half blocks, upper
+  LCD_Chr_Cp(0b00011111);
+  LCD_Chr_Cp(0b00011111);
+  LCD_Chr_Cp(0b00011111);
+  LCD_Chr_Cp(0b00011111);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+
+  // Half blocks, lower
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00000000);
+  LCD_Chr_Cp(0b00011111);
+  LCD_Chr_Cp(0b00011111);
+  LCD_Chr_Cp(0b00011111);
+  LCD_Chr_Cp(0b00011111);
+
+  LCD_RS = 0;
+  LCD_Cmd(128);
+  LCD_RS = 1;
+}
