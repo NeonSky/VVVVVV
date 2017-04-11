79d0e63db132e8ddb28d91c4c73894aac0ebcdde
Jacob7798 <jacob.a.eriksson@gmail.com>
2017-04-11 13:59:41 +0200

    Anton ruined my code = (

--- GameProject/gamecode.c
+++ GameProject/gamecode.c
@@ -60,15 +60,37 @@ char levels[2][lcdHeight][lcdWidth] = {
   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}},
 };
 
+static const char numberTiles = 2;
+static const char lcdCharLength = 8;
+static const char tiles[numberTiles][lcdCharLength] = {
+  {0b00001010,
+   0b00000000,
+   0b00010001,
+   0b00001110,
+   0b00000000,
+   0b00000000,
+   0b00000000,
+   0b00000000},
+
+  {0b00000000,
+   0b00000000,
+   0b00000000,
+   0b00000000,
+   0b00011111,
+   0b00011111,
+   0b00011111,
+   0b00011111}
+};
+
 void updatePlayerChar(char id1, char id2) {
-  char combinedTile[8];
-  for(i = 0; i < 8; i++) {
+  char combinedTile[lcdCharLength];
+  for(i = 0; i < lcdCharLength; i++) {
     combinedTile[i] = tiles[id1][i] | tiles[id2][i];
   }
   Lcd_RS = 0;
   Lcd_Cmd(64);
   Lcd_RS = 1;
-  for(i = 0; i < 8; i++) { Lcd_Chr_Cp(combinedTile[i]); }
+  for(i = 0; i < lcdCharLength; i++) { Lcd_Chr_Cp(combinedTile[i]); }
   Lcd_RS = 0;
   Lcd_Cmd(128);
   Lcd_RS = 1;
@@ -109,7 +131,7 @@ void checkAirborne() {
 }
 
 void update() {
-  checkAirborne();
+  /*checkAirborne();
   if(!player.isAirborne) {
     if(leftBtn) {
       if(getBlock(player.x-1, player.y) == Block.air) {
@@ -126,7 +148,12 @@ void update() {
     }
   }
   if(isAirborne) { player.y--; }
-  //checkCurTile(); // Check for goal, spikes etc.
+  //checkCurTile(); // Check for goal, spikes etc.*/
+
+  Lcd_Cmd(_LCD_CLEAR);
+  updatePlayerChar(1);
+  Lcd_Chr(1, 1);
+
   delay_ms(100);
 }
 
@@ -171,8 +198,8 @@ void initLCD() {
   Lcd_Cmd(64);
   Lcd_RS = 1;
 
-  for(i = 0; i < tilesCount; i++) {
-    for(j = 0; j < 8; j++) { Lcd_Chr_Cp(tiles[i][j]); }
+  for(i = 0; i < numberTiles; i++) {
+    for(j = 0; j < lcdCharLength; j++) { Lcd_Chr_Cp(tiles[i][j]); }
   }
 
   // Player, face-up_upper
